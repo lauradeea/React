b96c8fdb4decf92974058b167f022932f75e1cdc
laura ard <du220565>
2021-12-18 17:36:10 +0200

    Listening to Events & Working with Event handles

---
+++ src/components/NewExpens/ExpensForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./ExpensForm.css";
+
+const ExpensForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number"
+            min="0.01"
+            max="1000"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+        <div className="new-expense__action">
+          <button type="submit">Add Expense</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ExpensForm;
---
+++ src/components/NewExpens/NewExpens.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NewExpens.css";
+
+import ExpenseForm from "./ExpensForm";
+
+const NewExpens = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpens;
