7bf3e45f3bf5a121cb13f8201be86ceea1672466
lauradeea <lauralaradeea22@gmail.com>
2022-08-24 23:49:04 +0300

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,19 +10,30 @@ function Expenses(props) {
     setFilterYear(selectedYear);
   };
 
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filterExpenses.length > 0) {
+    expensesContent = filterExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
-    <div className="expenses test">
+    <div className="expenses">
       <ExpensesFilter
         selected={filterYear}
         onSaveSelect={filterChnageHandler}
       />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </div>
   );
 }
