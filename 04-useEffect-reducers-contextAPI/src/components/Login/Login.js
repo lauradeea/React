79769b1beb0394a99a8531af124ae08523cbef4b
lauradeea <lauralaradeea22@gmail.com>
2022-09-20 23:22:58 +0300

    Build & using a custom context provider component / remove everything from app and add context to index.js

--- 04-useEffect-reducers-contextAPI/src/components/Login/Login.js
+++ 04-useEffect-reducers-contextAPI/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -38,6 +39,7 @@ const Login = (props) => {
     value: "",
     isValid: false,
   });
+  const ctx = useContext(AuthContext);
 
   //object destructuring
   const { isValid: emailIsValid } = emailState;
@@ -75,7 +77,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
