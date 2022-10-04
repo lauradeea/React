0b8bb4dca90cb942a4567b0736dd16c7cbc6df8e
lauradeea <lauralaradeea22@gmail.com>
2022-10-04 22:21:48 +0300

    add cart for modal and css

---
+++ 05-food-order-app/src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  return (
+    <>
+      <div></div>
+      <div></div>
+      <div></div>
+    </>
+  );
+};
+
+export default Cart;
