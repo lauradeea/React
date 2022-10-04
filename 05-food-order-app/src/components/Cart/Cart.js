055a4c6138b10ad8c79767e1632fa556aa1c664d
lauradeea <lauralaradeea22@gmail.com>
2022-10-04 22:31:59 +0300

    add cart with dummy data and without modal

--- 05-food-order-app/src/components/Cart/Cart.js
+++ 05-food-order-app/src/components/Cart/Cart.js
@@ -1,13 +1,21 @@
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartItems = <ul className={classes['cart-items']}>{[{ id: 'c1', name:'Sushi',amount:2,price:12.99}].map(item =><li>{item.name}</li>)}</ul>;
   return (
     <>
-      <div></div>
-      <div></div>
-      <div></div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
     </>
   );
 };
 
 export default Cart;
+.
