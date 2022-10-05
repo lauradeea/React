b197a738465a19a9f799ef8132e2fe772e278e05
lauradeea <lauralaradeea22@gmail.com>
2022-10-05 20:23:32 +0300

    add portal not finished yet

--- 05-food-order-app/src/components/UI/Modal.js
+++ 05-food-order-app/src/components/UI/Modal.js
@@ -1,4 +1,26 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
-const Modal = (props) => {};
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>)}
+    </Fragment>
+  );
+};
 
 export default Modal;
