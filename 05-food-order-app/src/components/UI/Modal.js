407310cf7f56afe1d1001d25e2188550275737f6
lauradeea <lauralaradeea22@gmail.com>
2022-10-06 22:43:30 +0300

    portal

--- 05-food-order-app/src/components/UI/Modal.js
+++ 05-food-order-app/src/components/UI/Modal.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"; //for a portal
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
