08be59b6d2c955ad6eccbb633b87a517bb949593
lauradeea <lauralaradeea22@gmail.com>
2022-10-12 22:15:40 +0300

    call cart

--- 05-food-order-app/src/App.js
+++ 05-food-order-app/src/App.js
@@ -2,9 +2,12 @@ import { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import Card from "./components/UI/Card";
 function App() {
   return (
     <div>
+      <CarT />
       <Header />
       <main>
         <Meals />
