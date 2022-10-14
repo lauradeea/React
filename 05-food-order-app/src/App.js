1169b24ee64460ba9aff43035dea8e67087c6b17
lauradeea <lauralaradeea22@gmail.com>
2022-10-14 23:06:55 +0300

    use state

--- 05-food-order-app/src/App.js
+++ 05-food-order-app/src/App.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
   return (
     <div>
       <Cart />
