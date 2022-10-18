06a79ac1242e5a3d5d6e5356f96478e1589ccd7d
lauradeea <lauralaradeea22@gmail.com>
2022-10-18 23:05:17 +0300

    different style to close the modal

--- 05-food-order-app/src/components/UI/Modal.js
+++ 05-food-order-app/src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import classes from "./Modal.module.css";
 
 //received props because will receive data
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -22,7 +22,10 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
