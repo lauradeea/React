ed92a3d1b0d0c5a73a05c1636541195a548f466b
lauradeea <lauralaradeea22@gmail.com>
2023-08-27 17:50:50 +0300

    select items from a list and print them

--- 06-search-filter/src/components/User.js
+++ 06-search-filter/src/components/User.js
@@ -1,7 +1,11 @@
 import classes from "./User.module.css";
 
 const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+  return (
+    <li className={classes.user} onClick={() => props.onUserClick(props.id)}>
+      {props.name}
+    </li>
+  );
 };
 
 export default User;
--- 06-search-filter/src/components/Users.js
+++ 06-search-filter/src/components/Users.js
@@ -5,16 +5,26 @@ import classes from "./Users.module.css";
 
 const Users = (props) => {
   const [showUsers, setShowUsers] = useState(true);
+  const [selectedUserId, setSelectedUserId] = useState(null);
 
   const toggleUsersHandler = () => {
     setShowUsers((curState) => !curState);
+  };
+
+  const handleUserClick = (userId) => {
+    setSelectedUserId(userId);
   };
 
   const usersList = (
     <div className={classes.scrollableList}>
       <ul>
         {props.users.map((user) => (
-          <User key={user.id} name={user.name} />
+          <User
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            onUserClick={handleUserClick}
+          />
         ))}
       </ul>
     </div>
@@ -26,6 +36,7 @@ const Users = (props) => {
         {showUsers ? "Hide" : "Show"} Cars
       </button>
       {showUsers && usersList}
+      {selectedUserId && <p>Selected user ID: {selectedUserId}</p>}
     </div>
   );
 };
